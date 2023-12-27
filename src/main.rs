edd95b5535dc25a8fd8346dd948d587603c2f588
Linus Unnebäck <linus@folkdatorn.se>
2023-12-27 15:50:49 +0100

    Add async io/executor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::time::Duration;
+
+use async_io::Timer;
+use async_executor::LocalExecutor;
+
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
@@ -7,4 +12,13 @@ fn main() {
     esp_idf_svc::log::EspLogger::initialize_default();
 
     log::info!("Hello, world!");
+
+    let executor = LocalExecutor::new();
+
+    async_io::block_on(executor.run(async {
+        loop {
+            Timer::after(Duration::from_secs(5)).await;
+            log::info!("Hello, loop!");
+        }
+    }))
 }
